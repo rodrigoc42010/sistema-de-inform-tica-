6eb066a19db21f2f459fceea8ae8f26e4f5e99fb
rodrigoc42010 <rodrigoc42010@hotmail.com>
2025-11-14 21:53:33 -0300

    atualização de erro 404

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -5,13 +5,11 @@ import { selectUser } from '../selectors/authSelectors';
 const PrivateRoute = ({ role }) => {
   const user = useSelector(selectUser);
 
-  // Se não estiver logado, redirecionar para login
   if (!user) {
     return <Navigate to='/login' />;
   }
 
-  // Se o papel do usuário não corresponder ao papel necessário, redirecionar para o dashboard apropriado
-  if (role && user.role !== role) {
+  if (role && user.role && user.role !== role) {
     return <Navigate to={`/${user.role}/dashboard`} />;
   }
 
